7d97a5eaf40c2a3a2cbf45c74a64e27382073e27
TylorS <tlsteinberger167@gmail.com>
2015-11-20 02:08:26 -0500

    style(): Change code style
    Change code style to match that of the rest of Motorcyclejs
    repos.

--- src/index.js
+++ src/index.js
@@ -2,143 +2,154 @@ import most from 'most'
 import hold from '@most/hold'
 import superagent from 'superagent'
 
-// ESLint doesn't like function complexity
-function optionsToSuperagent({ // eslint-disable-line
-  url,
-  send = null,
-  accept = null,
-  query = null,
-  user = null,
-  password = null,
-  field = null,
-  attach = null, // if valid, should be an array
-  withCredentials = false,
-  headers = {},
-  redirects = 5,
-  type = `json`,
-  method = `get`,
-}) {
-  if (typeof url !== `string`) {
-    throw new Error(`Please provide a \`url\` property in the request options.`)
-  }
-  const lowerCaseMethod = method.toLowerCase()
-  const sanitizedMethod = lowerCaseMethod === `delete` ? `del` : lowerCaseMethod
-
-  let request = superagent[sanitizedMethod](url)
-  if (typeof request.redirects === `function`) {
-    request = request.redirects(redirects)
-  }
-  request = request.type(type)
-  if (send !== null) {
-    request = request.send(send)
-  }
-  if (accept !== null) {
-    request = request.accept(accept)
-  }
-  if (query !== null) {
-    request = request.query(query)
-  }
-  if (withCredentials) {
-    request = request.withCredentials()
-  }
-  if (user !== null && password !== null) {
-    request = request.auth(user, password)
-  }
-  for (let key in headers) {
-    if (headers.hasOwnProperty(key)) {
-      request = request.set(key, headers[key])
+const optionsToSuperagent =
+  ({ // eslint-disable-line
+    // ESLint doesn't like function complexity
+    url,
+    send = null,
+    accept = null,
+    query = null,
+    user = null,
+    password = null,
+    field = null,
+    attach = null, // if valid, should be an array
+    withCredentials = false,
+    headers = {},
+    redirects = 5,
+    type = `json`,
+    method = `get`,
+  }) => {
+    if (typeof url !== `string`) {
+      throw new Error(`Please provide a \`url\` property in the request` +
+        `options.`)
     }
-  }
-  if (field !== null) {
-    for (let key in field) {
-      if (field.hasOwnProperty(key)) {
-        request = request.field(key, field[key])
+    const lowerCaseMethod = method.toLowerCase()
+    const sanitizedMethod =
+      lowerCaseMethod === `delete` ?
+        `del` :
+        lowerCaseMethod
+
+    let request = superagent[sanitizedMethod](url)
+    if (typeof request.redirects === `function`) {
+      request = request.redirects(redirects)
+    }
+    request = request.type(type)
+    if (send !== null) {
+      request = request.send(send)
+    }
+    if (accept !== null) {
+      request = request.accept(accept)
+    }
+    if (query !== null) {
+      request = request.query(query)
+    }
+    if (withCredentials) {
+      request = request.withCredentials()
+    }
+    if (user !== null && password !== null) {
+      request = request.auth(user, password)
+    }
+    for (let key in headers) {
+      if (headers.hasOwnProperty(key)) {
+        request = request.set(key, headers[key])
       }
     }
-  }
-  if (attach !== null) {
-    for (let i = attach.length - 1; i >= 0; i--) {
-      const a = attach[i]
-      request = request.attach(a.name, a.path, a.filename)
+    if (field !== null) {
+      for (let key in field) {
+        if (field.hasOwnProperty(key)) {
+          request = request.field(key, field[key])
+        }
+      }
     }
-  }
-  return request
-}
-
-function urlToSuperagent(url) {
-  return superagent.get(url)
-}
-
-function createResponse$(reqOptions) {
-  return most.create((add, end, error) => {
-    let request
-    if (typeof reqOptions === `string`) {
-      request = urlToSuperagent(reqOptions)
-    } else if (typeof reqOptions === `object`) {
-      request = optionsToSuperagent(reqOptions)
-    } else {
-      error(new Error(`Observable of requests given to HTTP ` +
-        `Driver must emit either URL strings or objects with parameters.`))
-      return () => {} // noop
+    if (attach !== null) {
+      for (let i = attach.length - 1; i >= 0; i--) {
+        const a = attach[i]
+        request = request.attach(a.name, a.path, a.filename)
+      }
     }
-    try {
-      request.end((err, res) => {
-        if (err) {
-          error(err)
+    return request
+  }
+
+const urlToSuperagent = url => superagent.get(url)
+
+const createResponse$ =
+  reqOptions =>
+    most.create(
+      (add, end, error) => {
+        let request
+        if (typeof reqOptions === `string`) {
+          request = urlToSuperagent(reqOptions)
+        } else if (typeof reqOptions === `object`) {
+          request = optionsToSuperagent(reqOptions)
         } else {
-          add(res)
-          end()
+          error(new Error(`Observable of requests given to HTTP ` +
+            `Driver must emit either URL strings or objects with parameters.`))
+          return () => {} // noop
         }
-      })
-    } catch (err) {
-      error(err)
-    }
 
-    return function onDispose() {
-      request.abort()
-    }
-  })
-}
-
-function isolateSource(response$$, scope) {
-  return response$$.filter(res$ =>
-    Array.isArray(res$.request._namespace) &&
-    res$.request._namespace.indexOf(scope) !== -1
-  )
-}
-
-function isolateSink(request$, scope) {
-  return request$.map(req => {
-    if (typeof req === `string`) {
-      return {url: req, _namespace: [scope]}
-    }
-    req._namespace = req._namespace || []
-    req._namespace.push(scope)
-    return req
-  })
-}
-
-function makeHTTPDriver({eager = false} = {eager: false}) {
-  return function httpDriver(request$) {
-    let _response$$ = request$
-      .map(reqOptions => {
-        let response$ = createResponse$(reqOptions)
-        if (eager || reqOptions.eager) {
-          response$ = hold(response$)
-          response$.drain()
+        try {
+          request.end((err, res) => {
+            if (err) {
+              error(err)
+            } else {
+              add(res)
+              end()
+            }
+          })
+        } catch (err) {
+          error(err)
         }
-        response$.request = reqOptions
-        return response$
-      })
-    _response$$.drain()
-    let response$$ = hold(_response$$)
-    response$$.isolateSource = isolateSource
-    response$$.isolateSink = isolateSink
-    return response$$
-  }
-}
 
-module.exports = {
+        return function onDispose() {
+          request.abort()
+        }
+      }
+    )
+
+const isolateSource =
+  (response$$, scope) =>
+    response$$.filter(
+      res$ => Array.isArray(res$.request._namespace) &&
+        res$.request._namespace.indexOf(scope) !== -1
+    )
+
+const isolateSink =
+  (request$, scope) =>
+    request$.map(
+      req => {
+        if (typeof req === `string`) {
+          return {url: req, _namespace: [scope]}
+        }
+        req._namespace = req._namespace || []
+        req._namespace.push(scope)
+        return req
+      }
+    )
+
+const makeHTTPDriver =
+  ({eager = false} = {eager: false}) =>
+    request$ => {
+      const _response$$ =
+        request$
+          .map(
+            reqOptions => {
+              let response$ = createResponse$(reqOptions)
+              if (eager || reqOptions.eager) {
+                response$ = hold(response$)
+                response$.drain()
+              }
+              response$.request = reqOptions
+              return response$
+            }
+          )
+      _response$$.drain()
+      let response$$ = hold(_response$$)
+      response$$.isolateSink = isolateSink
+      response$$.isolateSource = isolateSource
+      return response$$
+    }
+
+export {
   optionsToSuperagent,
   urlToSuperagent,
   createResponse$,
